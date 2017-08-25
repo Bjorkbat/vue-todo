3abc191308b6217c6c6b96342506bbcda3fd66f3
Bjorkbat <bjorkbat@gmail.com>
2017-08-25 14:30:56 -0600

    Added an edit function, plus a ton of comments

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
   <div id="app">
 
     <form v-on:submit.prevent="addToDo">
+      <!-- Keeping it simple with the form.  It has a single input element.
+        Notice that it has a "v-model" directive to bind it to the "todo"
+        attribute returned by the data function -->
       <input type="text"
         placeholder="Type your new to-do here"
         v-model="todo">
@@ -23,20 +26,36 @@
 </template>
 
 <script>
-import TodoItem from './TodoItem.vue'
+
+// You can import other vue components like so.
+import TodoItem from './TodoItem.vue';
+
 export default {
   name: 'app',
+
+  // Vue components can import other components.  That's what we're doing here
+  // with TodoItem
   components: { TodoItem },
+
+  // Data contains the internal bits and pieces of data used by our component.
+  // It's a little odd, but data is a function, not an object, but we treat it
+  // a lot like an object.
   data () {
+
+    // All our data function does is return an object with some fairly simple
+    // attributes attached to it.
     return {
       todo: '',
       todos: [],
       msg: 'Welcome to Your Vue.js App'
     }
+
   },
 
   methods: {
 
+    // Adds new todos to our list.  Pretty simple really.  Just adds the
+    // todo from the form to an array and clears the form input
     addToDo: function() {
 
       // Push the todo typed inside the input into an array called todos
@@ -45,11 +64,14 @@ export default {
 
     },
 
+    // Removes todos from our list.  Triggered by a click event
     deleteToDo: function(todo) {
 
       for(var i = 0; i < this.todos.length; i ++) {
         if (this.todos[i] == todo) {
-          // Delete this todo (and just this one) from the array
+          // Delete this todo (and just this one) from the array using the
+          // splice method
+          // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
           this.todos.splice(i, 1);
         }
       }
@@ -61,6 +83,11 @@ export default {
 </script>
 
 <style lang="scss">
+/**
+ * Remember, you can add css (or in this case, scss) directly inside a
+ * component.  It's often a good idea to do so too, since most of the time a
+ * particular snippet of CSS only affects a particular Vue component
+ */
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/TodoItem.vue
+++ src/TodoItem.vue
@@ -1,9 +1,16 @@
 <template>
   <li>
+    <!-- This component is simply a list item element with some link tags
+      thrown in.  Still, it's better "componentize" this stuff rather than
+      repeat yourself -->
 
+    <!-- Here we're using "mustaches" to render the value of todo as text inside
+      our html. -->
     {{ todo }}
 
-    <a href="#">Edit</a>
+    <!-- we can attach v-on.click to our elements to register a function when
+      a click event is triggered -->
+    <a href="#" v-on:click.prevent="editToDo">Edit</a>
     <a href="#" v-on:click.prevent="deleteToDo">Delete</a>
 
   </li>
@@ -12,12 +19,25 @@
 <script>
 export default {
   name: "TodoItem",
+
+  // Here we can define what sort of props a vue component can accept
+  // from parent components.  In this case, our TodoItem just accepts a todo
+  // prop (which we can pass into this component using v-bind:todo)
   props: ['todo'],
 
   methods: {
 
+    // We're not going to delete the todo here.  Instead, we're just going to
+    // trigger a special event, namely a delete event.
     deleteToDo: function() {
       this.$emit('delete', this.todo);
+    },
+
+    // Just like with deleteTodo, we're going to emit an edit event when this
+    // function is called, and let the parent handle the business of editing
+    // the todo
+    editToDo: function() {
+      this.$emit('edit', this.todo);
     }
 
   }
